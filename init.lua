5e3777442449ac007ca71a201bd7d50ee5d9697d
Casimir <jul.lutz@gmx.net>
2015-08-15 22:23:54 +0200

    Extend definitinos

--- init.lua
+++ init.lua
@@ -1,27 +1,48 @@
 local player_pos = {}
 local timer = 0
 
+-- Area radius around the player within the sounds nodes get scanned.
 local AREA = 8
 
-local sounds={
-	{name = "air",	sound = ""},
-	{name = "fire:basic_flame",	sound = "areasound_fire"},
-	{name = "default:lava_source",	sound = "areasound_lava"},
-	{name = "default:lava_flowing",	sound = "areasound_lava"},
-	{name = "default:water_flowing",sound = "areasound_water_flow"},
-	{name = "default:grass_1",	sound = "areasound_crickets"},
-	{name = "default:grass_2",	sound = "areasound_crickets"},
-	{name = "default:grass_3",	sound = "areasound_crickets"},
-	{name = "default:grass_4",	sound = "areasound_crickets"},
-	{name = "default:grass_5",	sound = "areasound_crickets"},
-	{name = "default:dry_scrub",	sound = "areasound_rattlesnake"},
+-- Standard definitions
+local crickets ={
+	sound = "areasound_crickets",
+	time_start = 0.8,
+	time_end = 0.2,
+}
+
+-- Assinging sounds to nodes
+local areasound.sounds={
+	{name = "fire:basic_flame",	def = {
+		sound = "areasound_fire"
+	}},
+	{name = "default:lava_source",	def = {
+		sound = "areasound_lava"
+	}},
+	{name = "default:lava_flowing",	def = {
+		sound = "areasound_lava"
+	}},
+	{name = "default:water_flowing",def = {
+		sound = "areasound_water_flow"
+	}},
+	{name = "default:water_source",def = {
+		sound = "areasound_water_flow"
+	}},
+	{name = "default:grass_1",	def = crickets},
+	{name = "default:grass_2",	def = crickets},
+	{name = "default:grass_3",	def = crickets},
+	{name = "default:grass_4",	def = crickets},
+	{name = "default:grass_5",	def = crickets},
+	{name = "default:dry_scrub",	def = {
+		sound = "areasound_rattlesnake"
+	}},
 }
 
 for _,item in ipairs(sounds) do
 	if minetest.registered_items[item.name] then
 		print("[areasound] 1 = "..item.name)
-		print("[areasound] 2 = "..item.sound)
-		minetest.override_item(item.name, {areasound = item.sound})
+		print("[areasound] 2 = "..item.def.sound)
+		minetest.override_item(item.name, {areasound = item.def})
 	end
 end
 
@@ -46,7 +67,7 @@ minetest.register_globalstep(function(dtime)
 		}
 		local r_name = minetest.get_node(r_pos).name
 --		print("[areasound] r_name = "..r_name)
-		local r_sound = minetest.registered_items[r_name].areasound
+		local r_sound = minetest.registered_items[r_name].areasound.sound
 		if r_sound then
 --			print("[areasound] r_sound = "..r_sound)
 			minetest.sound_play(r_sound, {pos=player_pos[player_name], max_hear_distance = AREA})
